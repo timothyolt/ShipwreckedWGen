ab82d43f52ff219d1f894710465150782af3684b
Timothy Oltjenburns <timothyolt@gmail.com>
2013-07-27 15:33:59 -0400

    Fixed floating sugar cane and tree trunks in rivers

--- src/main/java/org/bytefire/plugins/shipwreckedwgen/populators/RiverPopulator.java
+++ src/main/java/org/bytefire/plugins/shipwreckedwgen/populators/RiverPopulator.java
@@ -14,6 +14,8 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.generator.BlockPopulator;
 
 import static org.bukkit.Material.*;
+import static org.bukkit.block.Biome.*;
+
 
 public class RiverPopulator extends BlockPopulator{
     private ShipwreckedWGen plugin;
@@ -27,9 +29,9 @@ public class RiverPopulator extends BlockPopulator{
         int cursorHeight = getHighestSolidBlockY(world, cursor.getBlockX(), cursor.getBlockZ());
         Biome cursorBiome = cursor.getBlock().getBiome();
         int chance;
-        if (cursorBiome == Biome.EXTREME_HILLS || cursorBiome == Biome.JUNGLE || cursorBiome == Biome.JUNGLE_HILLS) chance = random.nextInt(24);
+        if (cursorBiome == EXTREME_HILLS || cursorBiome == JUNGLE || cursorBiome == JUNGLE_HILLS) chance = random.nextInt(24);
         else chance = random.nextInt(48);
-        if (cursorHeight >= 100 && chance == 1 && cursorBiome != Biome.OCEAN && cursorBiome != Biome.FROZEN_OCEAN && cursorBiome != Biome.RIVER && cursorBiome != Biome.FROZEN_RIVER){
+        if (cursorHeight >= 100 && chance == 1 && cursorBiome != OCEAN && cursorBiome != FROZEN_OCEAN && cursorBiome != RIVER && cursorBiome != FROZEN_RIVER){
             cursor.setY(getHighestSolidBlockY(world, cursor.getBlockX(), cursor.getBlockZ()));
             int size = 16;
             chain: for (int i = 0; i < 16; i++){
@@ -40,7 +42,7 @@ public class RiverPopulator extends BlockPopulator{
                     else break chain;
                 }
                 else cursor = newCursor;
-                if (cursor.getBlock().getBiome() == Biome.OCEAN || cursor.getBlock().getBiome() == Biome.FROZEN_OCEAN) break chain;
+                if (cursor.getBlock().getBiome() == OCEAN || cursor.getBlock().getBiome() == FROZEN_OCEAN) break chain;
             }
         }
     }
@@ -53,14 +55,14 @@ public class RiverPopulator extends BlockPopulator{
             Biome biome;
             height = getHighestSolidBlockY(world, center.getBlockX() + x, center.getBlockZ() - size);
             biome = world.getBiome(center.getBlockX() + x, center.getBlockZ() - size);
-            if (height < lowEdge.getY() || biome == Biome.OCEAN || biome == Biome.FROZEN_OCEAN){
+            if (height < lowEdge.getY() || biome == OCEAN || biome == FROZEN_OCEAN){
                 lowEdge.setX(center.getBlockX() + x);
                 lowEdge.setZ(center.getBlockZ() - size);
                 lowEdge.setY(height);
             }
             height = getHighestSolidBlockY(world, center.getBlockX() + x, center.getBlockZ() + size);
             biome = world.getBiome(center.getBlockX() + x, center.getBlockZ() + size);
-            if (height < lowEdge.getY() || biome == Biome.OCEAN || biome == Biome.FROZEN_OCEAN){
+            if (height < lowEdge.getY() || biome == OCEAN || biome == FROZEN_OCEAN){
                 lowEdge.setX(center.getBlockX() + x);
                 lowEdge.setZ(center.getBlockZ() + size);
                 lowEdge.setY(height);
@@ -71,14 +73,14 @@ public class RiverPopulator extends BlockPopulator{
             Biome biome;
             height = getHighestSolidBlockY(world, center.getBlockX() - size, center.getBlockZ() + z);
             biome = world.getBiome(center.getBlockX() - size, center.getBlockZ() + z);
-            if (height < lowEdge.getY() || biome == Biome.OCEAN || biome == Biome.FROZEN_OCEAN){
+            if (height < lowEdge.getY() || biome == OCEAN || biome == FROZEN_OCEAN){
                 lowEdge.setX(center.getBlockX() - size);
                 lowEdge.setZ(center.getBlockZ() + z);
                 lowEdge.setY(height);
             }
             height = getHighestSolidBlockY(world, center.getBlockX() + size, center.getBlockZ() + z);
             biome = world.getBiome(center.getBlockX() + size, center.getBlockZ() + z);
-            if (height < lowEdge.getY() || biome == Biome.OCEAN || biome == Biome.FROZEN_OCEAN){
+            if (height < lowEdge.getY() || biome == OCEAN || biome == FROZEN_OCEAN){
                 lowEdge.setX(center.getBlockX() + size);
                 lowEdge.setZ(center.getBlockZ() + z);
                 lowEdge.setY(height);
@@ -88,7 +90,7 @@ public class RiverPopulator extends BlockPopulator{
     }
 
     void fillRiverSection(Location start, Location end, int size, Random rand){
-        fillRiverWaypoint(start, size);
+        fillRiverWaypoint(start, size, rand);
         int xdif = end.getBlockX() - start.getBlockX();
         int zdif = end.getBlockZ() - start.getBlockZ();
         if (Math.abs(xdif) > Math.abs(zdif)){
@@ -127,16 +129,17 @@ public class RiverPopulator extends BlockPopulator{
         }
     }
 
-    void fillRiverWaypoint(Location center, int size){
+    void fillRiverWaypoint(Location center, int size, Random rand){
         for (int x = (size * -1) - 1; x < size + 1; x++) for (int z = (size * -1) - 1; z < size + 1; z++){
             for (int y = (size * -1) - 1; y < size + 1; y++){
                 Location target = new Location(center.getWorld(), center.getBlockX() + x, center.getBlockY() + y, center.getBlockZ() + z);
                 Block targetBlock = target.getBlock();
                 double dist = center.distance(target);
+                double radi = center.distance(new Location(center.getWorld(), center.getBlockX() + x, center.getBlockY(), center.getBlockZ() + z));
                 Biome biome = targetBlock.getBiome();
-                if (biome != Biome.OCEAN && biome != Biome.FROZEN_OCEAN){
-                    if (biome == Biome.TAIGA || biome == Biome.TAIGA_HILLS || biome == Biome.ICE_PLAINS || biome == Biome.ICE_MOUNTAINS) targetBlock.setBiome(Biome.FROZEN_RIVER);
-                    else targetBlock.setBiome(Biome.RIVER);
+                if (biome != OCEAN && biome != FROZEN_OCEAN){
+                    if (biome == TAIGA || biome == TAIGA_HILLS || biome == ICE_PLAINS || biome == ICE_MOUNTAINS) targetBlock.setBiome(FROZEN_RIVER);
+                    else targetBlock.setBiome(RIVER);
                     if (dist < size){
                         if (y < 0) targetBlock.setType(WATER);
                         else targetBlock.setType(AIR);
@@ -144,6 +147,19 @@ public class RiverPopulator extends BlockPopulator{
                     else if (dist <= size + 2 && !targetBlock.isLiquid()){
                         if (y < -1) targetBlock.setType(DIRT);
                         else if (y < 0) targetBlock.setType(GRASS);
+                    }
+                    if (y > size - 1 && radi <= size + 1 && rand.nextInt(8) == 1){
+                        int height = -1;
+                        int id = rand.nextInt();
+                        heightCheck: for (int s = 0; s < 8; s++){
+                            if (target.getBlock().getRelative(0, -1 * (s), 0).getType() == GRASS){
+                                height = s;
+                                break heightCheck;
+                            }
+                        }
+                        if (height != -1) for (int s = 0; s < height; s++){
+                            target.getBlock().getRelative(0, -1 * (s), 0).setTypeId(SUGAR_CANE_BLOCK.getId(), false);
+                        }
                     }
                 }
             }
@@ -158,37 +174,46 @@ public class RiverPopulator extends BlockPopulator{
             Block targetBlock = target.getBlock();
             double dist = center.distance(target);
             Biome biome = targetBlock.getBiome();
-            if (biome != Biome.OCEAN && biome != Biome.FROZEN_OCEAN){
-                if (biome == Biome.TAIGA || biome == Biome.TAIGA_HILLS || biome == Biome.ICE_PLAINS || biome == Biome.ICE_MOUNTAINS) targetBlock.setBiome(Biome.FROZEN_RIVER);
-                else targetBlock.setBiome(Biome.RIVER);
+            if (biome != OCEAN && biome != FROZEN_OCEAN){
+                if (biome == TAIGA || biome == TAIGA_HILLS || biome == ICE_PLAINS || biome == ICE_MOUNTAINS) targetBlock.setBiome(FROZEN_RIVER);
+                else targetBlock.setBiome(RIVER);
                 if (dist < size){
                     if (y < 0) targetBlock.setType(WATER);
                     else {
                         if (y == size - 1){
                             int vineChance;
-                            if (biome == Biome.JUNGLE || biome == Biome.JUNGLE_HILLS) vineChance = rand.nextInt(4);
+                            if (biome == JUNGLE || biome == JUNGLE_HILLS) vineChance = rand.nextInt(4);
                             else vineChance = rand.nextInt(16);
                             Block vineTarget = target.getWorld().getBlockAt(target.getBlockX(), target.getBlockY(), target.getBlockZ());
                             if (!vineTarget.getRelative(BlockFace.UP).isEmpty() && vineChance == 1) vineTarget.setTypeIdAndData(106, (byte)1, false);
-                            else targetBlock.setType(AIR);
+                            else clearAndPatch(targetBlock);
                         }
-                        else targetBlock.setType(AIR);
+                        else clearAndPatch(targetBlock);
                     }
                 }
                 else if (!targetBlock.isLiquid()){
                     if (dist <= size + 2){
                         if (y < -1) targetBlock.setType(DIRT);
                         else if (y < 0) targetBlock.setType(GRASS);
-                        else if (y >= 0 && (targetBlock.getType() == DIRT || targetBlock.getType() == GRASS) && targetBlock.getRelative(BlockFace.DOWN).getType() == AIR) targetBlock.setType(AIR);
-                        if (y == 0 && rand.nextInt(48) == 1 && !targetBlock.getRelative(BlockFace.DOWN).isEmpty()){
-                            int caneHeight = rand.nextInt(2) + 2;
-                            for (int cy = 0; cy <= caneHeight; cy++) targetBlock.getRelative(0, cy, 0).setType(SUGAR_CANE_BLOCK);
-                        }
+                        else if (y >= 0 && (targetBlock.getType() == DIRT || targetBlock.getType() == GRASS) && targetBlock.getRelative(BlockFace.DOWN).getType() == AIR) clearAndPatch(targetBlock);
                     }
                     else if (y < 0 && targetBlock.isEmpty()) targetBlock.setType(STONE);
                 }
             }
         }
+    }
+
+    private void clearAndPatch(Block targetBlock){
+        if (targetBlock.getType() == LOG){
+            for (int p = 1; p < 16; p++){
+                Block patchBlock = targetBlock.getRelative(0, -1 * p, 0);
+                switch (patchBlock.getType()){
+                    case AIR: case WATER: case STATIONARY_WATER: case LEAVES: case LOG:
+                        patchBlock.setTypeIdAndData(LOG.getId(), targetBlock.getData(), false);
+                }
+            }
+        }
+        else targetBlock.setType(AIR);
     }
 
     private int getHighestSolidBlockY(World world, int x, int z){
