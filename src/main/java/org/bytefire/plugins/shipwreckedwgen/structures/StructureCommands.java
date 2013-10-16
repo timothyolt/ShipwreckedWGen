51cdc2449867f0bb29bdde1c45c9f90aedfe1ae1
Timothy Oltjenburns <timothyolt@gmail.com>
2013-10-16 15:39:56 -0400

    Added help command interface

--- src/main/java/org/bytefire/plugins/shipwreckedwgen/structures/StructureCommands.java
+++ src/main/java/org/bytefire/plugins/shipwreckedwgen/structures/StructureCommands.java
@@ -20,6 +20,23 @@ import org.bytefire.plugins.shipwreckedwgen.structures.Structure.StructureType;
 
 public class StructureCommands implements CommandExecutor{
 
+    public static final String structSyntax = "/struct <command>";
+        public static final String structLoadSyntax = "/struct load <structure>";
+        public static final String structTpSyntax = "/struct tp <structure>";
+        public static final String structSaveSyntax = "/struct save [structure]";
+        public static final String structSetSyntax = "/struct set <attribute>";
+            public static final String structSetOriginSyntax = "/struct set origin [structure]";
+            public static final String structSetDistanceSyntax = "/struct set distance <distance> [structure]";
+            public static final String structSetChanceSyntax = "/struct set chance <chance> [structure]";
+            public static final String structSetBiomeSyntax = "/struct set biome <biome> [structure]";
+            public static final String structSetTypeSyntax = "/struct set type <type> [structure]";
+            public static final String structSetMaxSyntax = "/struct set max <max> [structure]";
+            public static final String structSetMinSyntax = "/struct set min <min> [structure]";
+            public static final String structSetGrowSyntax = "/struct set grow <grow> [structure]";
+        public static final String structPassiveSyntax = "/struct passive <operation>";
+             public static final String structPassiveMaterialSyntax = "/struct passive material <material> <passive> [structure]";
+        public static final String structHelpSyntax = "/struct help [command]";
+
     private ShipwreckedWGen plugin;
 
     public StructureCommands(ShipwreckedWGen plugin){
@@ -27,17 +44,48 @@ public class StructureCommands implements CommandExecutor{
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String str, String[] args) {
+        return cmdStruct(sender, args, false);
+    }
+
+    public boolean cmdStruct(CommandSender sender, String[] args, boolean help){
+        if (sender.isOp() != true) {
+            error(sender, "You do not have permission for that!");
+            return true;
+        }
+
+        if (help && args.length < 1){
+            message(sender, "Syntax: " + structSyntax);
+            message(sender, "Sub-Commands:");
+            message(sender, structLoadSyntax);
+            message(sender, structTpSyntax);
+            message(sender, structSaveSyntax);
+            message(sender, structSetSyntax);
+            message(sender, structPassiveSyntax);
+            message(sender, structHelpSyntax);
+            message(sender, "The base of every command with anything to do with structures.");
+            return true;
+        }
+
         if (args.length < 1) error(sender, "No operation specified");
-        if      (args[0].equals("load")) return cmdLoad(sender, args);
-        else if (args[0].equals("tp"))   return cmdTp(sender, args);
-        else if (args[0].equals("save")) return cmdSave(sender, args);
-        else if (args[0].equals("set")) return cmdSet(sender, args);
-        else if (args[0].equals("passive")) return cmdPassive(sender, args);
+        if      (args[0].equals("load")) return cmdLoad(sender, args, help);
+        else if (args[0].equals("tp"))   return cmdTp(sender, args, help);
+        else if (args[0].equals("save")) return cmdSave(sender, args, help);
+        else if (args[0].equals("set")) return cmdSet(sender, args, help);
+        else if (args[0].equals("passive")) return cmdPassive(sender, args, help);
+        else if (args[0].equals("help")) return cmdHelp(sender, args, help);
         else error(sender, "Not a valid operation");
         return true;
     }
 
-    public boolean cmdLoad(CommandSender sender, String[] args){
+    public boolean cmdLoad(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structLoadSyntax);
+            message(sender, "Loads a structure file from the plugin directory with the given name");
+            message(sender, "Note: The '.structure' at the end of the file should NOT be included");
+            message(sender, "Ex: /struct load desert1");
+            return true;
+        }
+
         if (args.length < 2) {
             error(sender, "No structure specified");
             return true;
@@ -61,7 +109,15 @@ public class StructureCommands implements CommandExecutor{
         }
     }
 
-    public boolean cmdTp(CommandSender sender, String[] args){
+    public boolean cmdTp(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structTpSyntax);
+            message(sender, "Teleports the player to the specified structure,");
+            message(sender, "loading it if necessary");
+            message(sender, "Ex: /struct tp desert1");
+            return true;
+        }
+
         if (!(sender instanceof Player)){
             error(sender, "You are not a player");
             return true;
@@ -72,7 +128,7 @@ public class StructureCommands implements CommandExecutor{
         }
         World destWorld = Bukkit.getServer().getWorld(args[1] + ".structure");
         if (destWorld == null){
-            cmdLoad(sender, args);
+            cmdLoad(sender, args, help);
             destWorld = Bukkit.getServer().getWorld(args[1] + ".structure");
             if (destWorld == null){
                 error(sender, "Structure " + args[1] + " could not be loaded");
@@ -93,7 +149,15 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSave(CommandSender sender, String[] args){
+    public boolean cmdSave(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSaveSyntax);
+            message(sender, "Saves the structure specified, or");
+            message(sender, "if none specified, the one the player is in");
+            message(sender, "Ex: /struct save desert1");
+            return true;
+        }
+
         Structure struct = getStructureFromSender(sender, args, 1);
         if (struct != null){
             message(sender, "Saving structure " + struct.getName() + " ...");
@@ -104,22 +168,46 @@ public class StructureCommands implements CommandExecutor{
         else return true;
     }
 
-    public boolean cmdSet(CommandSender sender, String[] args){
+    public boolean cmdSet(CommandSender sender, String[] args, boolean help){
+        if (help && args.length < 2){
+            message(sender, "Syntax: " + structSetSyntax);
+            message(sender, "Sub-Commands:");
+            message(sender, structSetOriginSyntax);
+            message(sender, structSetDistanceSyntax);
+            message(sender, structSetChanceSyntax);
+            message(sender, structSetBiomeSyntax);
+            message(sender, structSetTypeSyntax);
+            message(sender, structSetMaxSyntax);
+            message(sender, structSetMinSyntax);
+            message(sender, structSetGrowSyntax);
+            message(sender, "Sets attributes of the structure specified, or");
+            message(sender, "if none specified, the one the player is in");
+            return true;
+        }
+
         String[] newargs = Arrays.copyOfRange(args, 1, args.length);
         if (args.length < 2) error(sender, "No operation specified");
-        if      (args[1].equals("origin")) return cmdSetOrigin(sender, newargs);
-        else if (args[1].equals("distance")) return cmdSetDistance(sender, newargs);
-        else if (args[1].equals("chance")) return cmdSetChance(sender, newargs);
-        else if (args[1].equals("biome"))  return cmdSetBiome(sender, newargs);
-        else if (args[1].equals("type"))   return cmdSetType(sender, newargs);
-        else if (args[1].equals("max"))    return cmdSetYMax(sender, newargs);
-        else if (args[1].equals("min"))    return cmdSetYMin(sender, newargs);
-        else if (args[1].equals("grow"))   return cmdSetGrow(sender, newargs);
+        if      (args[1].equals("origin")) return cmdSetOrigin(sender, newargs, help);
+        else if (args[1].equals("distance")) return cmdSetDistance(sender, newargs, help);
+        else if (args[1].equals("chance")) return cmdSetChance(sender, newargs, help);
+        else if (args[1].equals("biome"))  return cmdSetBiome(sender, newargs, help);
+        else if (args[1].equals("type"))   return cmdSetType(sender, newargs, help);
+        else if (args[1].equals("max"))    return cmdSetYMax(sender, newargs, help);
+        else if (args[1].equals("min"))    return cmdSetYMin(sender, newargs, help);
+        else if (args[1].equals("grow"))   return cmdSetGrow(sender, newargs, help);
         else error(sender, "Not a valid operation");
         return true;
     }
 
-    public boolean cmdSetOrigin(CommandSender sender, String[] args){
+    public boolean cmdSetOrigin(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetOriginSyntax);
+            message(sender, "Sets the origin of the structure specified to the player's");
+            message(sender, "coordinates, or if none specified, the one the player is in");
+            message(sender, "Ex: /struct set origin");
+            return true;
+        }
+
         Structure struct = getStructureFromSender(sender, args, 1);
         Player player = getPlayerFromSender(sender, args, 2);
         if (struct == null || player == null) return true;
@@ -133,7 +221,15 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSetDistance(CommandSender sender, String[] args){
+    public boolean cmdSetDistance(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetDistanceSyntax);
+            message(sender, "Sets the minimum chunk spawn distance of the structure");
+            message(sender, "specified, or if none specified, the one the player is in");
+            message(sender, "Ex: /struct set distance 16");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No minimum distance specified");
             return true;
@@ -146,7 +242,17 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSetChance(CommandSender sender, String[] args){
+    public boolean cmdSetChance(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetChanceSyntax);
+            message(sender, "Sets the spawn chance of the structure specified,");
+            message(sender, "or if none specified, the one the player is in");
+            message(sender, "Note: a grid is constructed by the distance and each section");
+            message(sender, "tests a random 1/chance to see if it will spawn");
+            message(sender, "Ex: /struct set chance 16");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No spawn chance specified");
             return true;
@@ -159,7 +265,16 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSetBiome(CommandSender sender, String[] args){
+    public boolean cmdSetBiome(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetBiomeSyntax);
+            message(sender, "Sets the spawn biome of the structure specified,");
+            message(sender, "or if none specified, the one the player is in");
+            message(sender, "Note: use \"null\" to spawn in any biome");
+            message(sender, "Ex: /struct set biome ocean");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No biome specified");
             return true;
@@ -186,7 +301,17 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSetType(CommandSender sender, String[] args){
+    public boolean cmdSetType(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetTypeSyntax);
+            message(sender, "Sets the spawn type of the structure specified,");
+            message(sender, "or if none specified, the one the player is in");
+            message(sender, "Note: \"surface\", \"sky\", \"underground\",");
+            message(sender, "and \"all\" may be used");
+            message(sender, "Ex: /struct set ocean");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No type specified");
             return true;
@@ -206,7 +331,16 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSetYMax(CommandSender sender, String[] args){
+    public boolean cmdSetYMax(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetMaxSyntax);
+            message(sender, "Sets the vertical spawn max of the structure specified,");
+            message(sender, "or if none specified, the one the player is in");
+            message(sender, "Note: calculated from the origin of the structure");
+            message(sender, "Ex: /struct set max 128");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No maximum height specified");
             return true;
@@ -219,7 +353,16 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSetYMin(CommandSender sender, String[] args){
+    public boolean cmdSetYMin(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetMinSyntax);
+            message(sender, "Sets the vertical spawn minimum of the structure specified,");
+            message(sender, "or if none specified, the one the player is in");
+            message(sender, "Note: calculated from the origin of the structure");
+            message(sender, "Ex: /struct set min 128");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No minimum height specified");
             return true;
@@ -232,7 +375,17 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdSetGrow(CommandSender sender, String[] args){
+    public boolean cmdSetGrow(CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structSetMaxSyntax);
+            message(sender, "Toggles if structures can grow outside of their set,");
+            message(sender, "boundaries. For example, the entire structure has to be");
+            message(sender, "in a kind of biome, or under a certain height.");
+            message(sender, "Note: " + ChatColor.RED + "currently unimplemented");
+            message(sender, "Ex: /struct set grow true");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No grow specified");
             return true;
@@ -245,16 +398,33 @@ public class StructureCommands implements CommandExecutor{
         return true;
     }
 
-    public boolean cmdPassive(CommandSender sender, String[] args){
+    public boolean cmdPassive(CommandSender sender, String[] args, boolean help){
+        if (help && args.length < 2){
+            message(sender, "Syntax: " + structPassiveSyntax);
+            message(sender, "Sub-Commands:");
+            message(sender, structPassiveMaterialSyntax);
+            message(sender, "Commands that modify the passive/assertive property of blocks");
+            return true;
+        }
+
         String[] newargs = Arrays.copyOfRange(args, 1, args.length);
         if (args.length < 2) error(sender, "No operation specified");
-        if      (args[1].equals("material")) return cmdPassiveMaterial(sender, newargs);
-        else if (args[1].equals("distance")) return cmdSetDistance(sender, newargs);
+        if      (args[1].equals("material")) return cmdPassiveMaterial(sender, newargs, help);
+        else if (args[1].equals("distance")) return cmdSetDistance(sender, newargs, help);
         else error(sender, "Not a valid operation");
         return true;
     }
 
-    public boolean cmdPassiveMaterial(final CommandSender sender, String[] args){
+    public boolean cmdPassiveMaterial(final CommandSender sender, String[] args, boolean help){
+        if (help){
+            message(sender, "Syntax: " + structPassiveMaterialSyntax);
+            message(sender, "Sets passive/assertive flag of all locations of a certain block");
+            message(sender, "Note: Changing the block will not modify the flag,");
+            message(sender, "so this is useful for making a section of assertive air");
+            message(sender, "Ex: /struct passive material stone true");
+            return true;
+        }
+
         if (args.length < 2){
             error(sender, "No material specified");
             return true;
@@ -314,6 +484,18 @@ public class StructureCommands implements CommandExecutor{
             }
         });
         return true;
+    }
+
+    public boolean cmdHelp(CommandSender sender, String[] args, boolean help){
+        if (help) {
+            message(sender, "Syntax: /struct help [command]");
+            message(sender, "The help command provides basic help on other commands.");
+            message(sender, "Ex: /struct help set biome");
+            return true;
+        }
+
+        String[] newargs = Arrays.copyOfRange(args, 1, args.length);
+        return cmdStruct(sender, newargs, true);
     }
 
     public Structure getStructureFromSender(CommandSender sender, String[] args, int index){
